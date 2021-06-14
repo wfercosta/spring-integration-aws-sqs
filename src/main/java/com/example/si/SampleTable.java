6576bd59897aa4ebbe91af43da07252ec5d7546c
Wander Costa <wanderfernando@gmail.com>
2021-06-14 17:04:56 -0300

    Test

--- src/main/java/com/example/si/SampleTable.java
+++ src/main/java/com/example/si/SampleTable.java
@@ -4,12 +4,16 @@ package com.example.si;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @DynamoDBTable(tableName = "sample")
+@NoArgsConstructor
+@AllArgsConstructor
 public class SampleTable {
 
 	@DynamoDBHashKey(attributeName = "organisation")
--- src/test/java/com/example/si/SampleRepositoryTest.java
+++ src/test/java/com/example/si/SampleRepositoryTest.java
@@ -2,17 +2,30 @@ package com.example.si;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 @ExtendWith(DynamoDBExtension.class)
 @SpringBootTest
+@Profile("test")
 public class SampleRepositoryTest {
 
 	@Autowired
@@ -25,5 +38,37 @@ public class SampleRepositoryTest {
 	public void test() {
 		assertThat(client, notNullValue());
 		assertThat(mapper, notNullValue());
+	}
+
+	@BeforeAll
+	public static void beforeAll() {
+
+	}
+
+	@Test
+	@DisplayName("Should write to persistence")
+	public void Should_WriteToPersistence() {
+
+		final String id = UUID.randomUUID().toString();
+		final SampleTable sample = SampleTable.builder().organisation(id)
+				.hash(UUID.randomUUID().toString())
+				.build();
+
+
+		mapper.save(sample);
+
+
+		final List<SampleTable> results = mapper.query(SampleTable.class,
+				new DynamoDBQueryExpression<SampleTable>()
+						.withConsistentRead(false)
+						.withExpressionAttributeValues(
+								Map.of(":id", new AttributeValue().withS(id))
+						)
+						.withKeyConditionExpression("organisation = :id"));
+
+		assertThat(results.size(), equalTo(1) );
+
+		mapper.delete(sample);
+
 	}
 }
